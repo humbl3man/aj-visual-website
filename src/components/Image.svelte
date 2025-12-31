8e26e9180c36b991ad06b1153908e07bd8689c21
Konstantin <humbl3dev@gmail.com>
2025-12-31 00:37:41 -0800

    feat: add Image component and utility for Sanity image URLs

---
+++ src/components/Image.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { SanityImageAssetReference } from '$lib/sanity.schema';
+	import { urlFor } from '$lib/sanityImageUrl';
+	import type { SanityAsset, SanityImageObject, SanityImageSource } from '@sanity/image-url';
+
+	const {
+		image,
+		asset,
+		alt,
+		width,
+		height
+	}: {
+		asset: SanityImageAssetReference;
+		image?: SanityImageObject;
+		width: number;
+		height: number;
+		alt?: string;
+	} = $props();
+</script>
+
+{#if asset}
+	<img src={urlFor(asset).width(width).height(height).url()} alt={alt || ''} />
+{:else if image}
+	<img src={urlFor(image).width(width).height(height).url()} alt={alt || ''} />
+{/if}
---
+++ src/lib/sanityImageUrl.ts
@@ -0,0 +1,10 @@
+// sanityImageUrl.ts
+import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url';
+
+import { client } from './sanity';
+
+const builder = createImageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+	return builder.image(source);
+}
