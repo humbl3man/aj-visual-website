75fa31aad5f2e4c1871e5b8dc58456cd9061b12c
Konstantin <humbl3dev@gmail.com>
2026-01-01 16:18:27 -0800

    refactor: clean up project page layout; enhance article structure and styles

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { getProjects } from '$lib/api/project.remote';
-	// import { PortableText } from '@portabletext/svelte';
-	// import { toHTML } from '@portabletext/to-html';
 	import Image from '../../components/Image.svelte';
 
 	const { projects } = await getProjects();
@@ -10,35 +8,27 @@
 <div class="container mx-auto max-w-7xl px-4">
 	<h1 class="mb-8 text-4xl font-semibold">Projects</h1>
 
-	<section class="grid grid-cols-2 gap-6 lg:grid-cols-3">
+	<section class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 		{#each projects as project}
 			{@const thumbnailImageAsset = project.images && project?.images[0].asset}
 			{@const url = project.slug?.current}
 			<a href={`/projects/${url}`}>
-				<article
-					class="group relative z-1 flex h-full w-full items-center justify-center overflow-hidden rounded-sm bg-gray-100"
-				>
+				<article class="group relative z-1 overflow-hidden">
 					<div
-						class="absolute top-[50%] left-[50%] z-20 min-w-[70%] translate-x-[-50%] translate-y-[-50%] p-1 transition-all duration-200 md:opacity-0 md:group-hover:opacity-100"
+						class="absolute top-[50%] left-[50%] z-10 flex -translate-x-[50%] -translate-y-[50%] flex-col gap-4"
 					>
-						<div class="mb-4 text-center text-lg font-bold md:text-2xl">{project.title}</div>
-						<!-- <div>
-							<PortableText value={project.description} />
-						</div> -->
+						<div class="text-center font-semibold text-white sm:text-xl md:text-2xl">
+							{project.title}
+						</div>
 					</div>
-					<div
-						class="opacity-50 transition-opacity duration-200 md:opacity-100 md:group-hover:opacity-50"
-					>
-						<Image asset={thumbnailImageAsset!} width={600} height={600} />
-					</div>
+					<Image
+						asset={thumbnailImageAsset!}
+						width={600}
+						height={600}
+						class="block h-full w-full object-cover brightness-50 transition duration-500 group-hover:brightness-75"
+					/>
 				</article>
 			</a>
 		{/each}
 	</section>
 </div>
-
-<style lang="postcss">
-	article img {
-		@apply absolute top-0 right-0 bottom-0 left-0 block h-full w-full;
-	}
-</style>
